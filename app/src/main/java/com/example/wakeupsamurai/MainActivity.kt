43f3ec9c19c5d85274f2cb160b39ce6887adf49c
thomas-d <tho.daskalakis@gmail.com>
2024-09-04 16:59:37 +0300

    update app to check and ask for permissions

--- app/src/main/java/com/example/wakeupsamurai/MainActivity.kt
+++ app/src/main/java/com/example/wakeupsamurai/MainActivity.kt
@@ -1,18 +1,37 @@
 package com.example.wakeupsamurai
 
+import android.Manifest
 import android.content.Context
 import android.os.Bundle
 import android.os.PowerManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
+import android.widget.Toast
+import android.content.pm.PackageManager
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var wakeLock: PowerManager.WakeLock
+    private val PERMISSIONS_REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Request necessary permissions
+        if (!hasPermissions()) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_WIFI_STATE,
+                    Manifest.permission.CHANGE_WIFI_STATE,
+                    Manifest.permission.WAKE_LOCK
+                ),
+                PERMISSIONS_REQUEST_CODE
+            )
+        }
 
         // Initialize WakeLock
         val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
@@ -29,6 +48,27 @@ class MainActivity : AppCompatActivity() {
 
         stopButton.setOnClickListener {
             stopHotspot()
+        }
+    }
+
+    private fun hasPermissions(): Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CHANGE_WIFI_STATE) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WAKE_LOCK) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                Toast.makeText(this, "Permissions Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permissions Denied", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
